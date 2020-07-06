2425cabad9cab2141fe9da04c00fce2f031f07c1
ahbarnett <ahb@math.dartmouth.edu>
2020-07-06 18:20:32 -0400

    {single,double} inputs for {add,addf}

--- singleprec/test.m
+++ singleprec/test.m
@@ -1,8 +1,10 @@
-% test the funcs in mwrapfloat
+% test the funcs in mwrapfloat. Barnett & Gimbutas.
 
 c = add(double(1/3),double(1/3)), class(c)   % double->double, as mwrap 0.33.3 designed for!
 
-c = addf(double(1/3),double(1/3)), class(c)   % should type-convert to single, output single
+c = add(single(1)/3,single(1)/3)   % should type-convert and give 1e-7 acc, or catch the error
+
+c = addf(double(1/3),double(1/3)), class(c)   % should type-convert to single, or catch error
 
 c = addf(single(1)/3,single(1)/3)   % input as designed, should give single
 
