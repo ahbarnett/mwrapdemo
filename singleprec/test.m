6c0b5ca030816c5f813265040bd2785841f80415
ahbarnett <ahb@math.dartmouth.edu>
2020-07-06 22:17:35 -0400

    test catches desired errors

--- singleprec/test.m
+++ singleprec/test.m
@@ -4,9 +4,19 @@ format compact
 
 c = add(double(1/3),double(1/3)), class(c)   % double->double, as mwrap 0.33.3 designed for!
 
-c = add(single(1)/3,single(1)/3)   % should type-convert and give 1e-7 acc, or catch the error
+try
+c = add(single(1)/3,single(1)/3)   % should error
+catch ME
+  ME.message
+  disp('good')
+end
 
-c = addf(double(1/3),double(1/3)), class(c)   % should type-convert to single, or catch error
+try
+c = addf(double(1/3),double(1/3)), class(c)   % should error
+catch ME
+  ME.message
+  disp('good')
+end
 
 c = addf(single(1)/3,single(1)/3)   % input as designed, should give single
 
