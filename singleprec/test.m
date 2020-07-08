33bb8232eb9a7d6c46c3e69d0891441aad61829d
ahbarnett <ahb@math.dartmouth.edu>
2020-07-08 13:19:46 -0400

    singleprec actual arrays to check complex ordering

--- singleprec/test.m
+++ singleprec/test.m
@@ -1,46 +1,51 @@
 % test the funcs in mwrapfloat. Barnett & Gimbutas.
+
+% *** should be made pass-fail. Need to check complex arrays correct by eye
+
 format long g
 format compact
 
 fprintf('scalar real routines...\n')
-c = add(double(1/3),double(1/3)), class(c)
+x = 1/3; xf = single(x);
+c = add(x,x), class(c)
 
 try
-c = add(single(1)/3,single(1)/3)   % should error
+c = add(xf,xf)   % should error
 catch ME
   ME.message
   disp('good')
 end
 
 try
-c = addf(double(1/3),double(1/3)), class(c)   % should error
+c = addf(x,x), class(c)   % should error
 catch ME
   ME.message
   disp('good')
 end
 
-c = addf(single(1)/3,single(1)/3)   % input as designed, should give single
-
+c = addf(xf,xf)   % input as designed, should give single
 
 
 fprintf('\narray real routines...\n')
-c = arradd(double(1/3),double(1/3)), class(c)   % double->double, as mwrap 0.33.3 designed for!
+x = x*ones(3,1); xf = xf*ones(3,1);
+
+c = arradd(x,x), class(c)   % double->double, as mwrap 0.33.3 designed for!
 
 try
-c = arradd(single(1)/3,single(1)/3)   % should error
+c = arradd(xf,xf)   % should error
 catch ME
   ME.message
   disp('good')
 end
 
 try
-c = arraddf(double(1/3),double(1/3)), class(c)   % should error
+c = arraddf(x,x), class(c)   % should error
 catch ME
   ME.message
   disp('good')
 end
 
-c = arraddf(single(1)/3,single(1)/3)   % input as designed, should give single
+c = arraddf(xf,xf)   % input as designed, should give single
 
 
 fprintf('\nscalar complex routines...\n')
@@ -64,6 +69,7 @@ end
 c = addc(zf,zf)   % input as designed, should give single
 
 fprintf('\narray complex routines...\n')
+z = z*ones(3,1); zf = zf*ones(3,1);
 
 try
   c = arraddz(z,z), class(c)     % input as designed, double
